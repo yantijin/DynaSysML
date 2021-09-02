70cbb85f92b589ab89d0df6965e1c165ee9c7fab
yantijin <yantijin@163.com>
2021-09-02 16:36:04 +0800

    change cond ddpm, add discrete_vdm demo

--- example/EBM/mnist_ddpm.py
+++ example/EBM/mnist_ddpm.py
@@ -5,57 +5,17 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from tqdm import tqdm
 from unet import Unet, Cond_Unet
-from DynaSysML.EBM.ddpm import gaussian_ddpm, cond_gaussian_ddpm, mix_gaussian_ddpm
-from DynaSysML.EBM.utils import cosine_beta_schedule, EMA
+from DynaSysML.EBM.ddpm import gaussian_ddpm, mix_gaussian_ddpm
+from DynaSysML.EBM.vdm import gau_vdm_ddpm
+from DynaSysML.EBM.utils import cosine_beta_schedule, EMA, extract
 import copy
 import numpy as np
 import matplotlib.pyplot as plt
+from utils import plot_fig, get_mnist_loaders
 
-def plot_fig(res):
-    n = 6
-    digit_size = 28
-    figure = np.zeros((28 * n, 28 * n))
-    for i in range(n):
-        for j in range(n):
-            figure[i * digit_size: (i + 1) * digit_size,
-            j * digit_size: (j + 1) * digit_size] = res[n * i + j, 0, :, :]
 
-    plt.figure()
-    plt.imshow(figure, cmap='gray')
 
-def get_mnist_loaders(data_aug=False, batch_size=128, test_batch_size=1000, perc=1.0):
-    if data_aug:
-        transform_train = transforms.Compose([
-            transforms.RandomCrop(28, padding=4),
-            transforms.ToTensor(),
-        ])
-    else:
-        transform_train = transforms.Compose([
-            transforms.ToTensor(),
-        ])
-
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-    ])
-
-    train_loader = DataLoader(
-        datasets.MNIST(root='../data/', train=True, download=True, transform=transform_train), batch_size=batch_size,
-        shuffle=True, num_workers=2, drop_last=True
-    )
-
-    train_eval_loader = DataLoader(
-        datasets.MNIST(root='../data/', train=True, download=True, transform=transform_test),
-        batch_size=test_batch_size, shuffle=False, num_workers=2, drop_last=True
-    )
-
-    test_loader = DataLoader(
-        datasets.MNIST(root='../data/', train=False, download=True, transform=transform_test),
-        batch_size=test_batch_size, shuffle=False, num_workers=2, drop_last=True
-    )
-
-    return train_loader, test_loader, train_eval_loader
-
-tp = 'mix_gau_ddpm'
+tp = 'cond_mix_gau_ddpm' #
 
 if tp == 'ddpm':
     epochs = 20
@@ -117,7 +77,7 @@ elif tp == 'cond_ddpm':
     model = Cond_Unet(dim=16, channels=1, dim_mults=(1, 2, 4)).cuda()
     # betas = cosine_beta_schedule(1000, )
     betas = np.linspace(1e-4, 1e-2, 1000)
-    diffusion = cond_gaussian_ddpm(model, loss_type='l2', betas=betas).cuda()
+    diffusion = gaussian_ddpm(model, loss_type='l2', betas=betas).cuda()
     ema_model = copy.deepcopy(model)
     optim = torch.optim.Adam(model.parameters(), lr=1e-3)
     train_loader, test_loader, val_loader = get_mnist_loaders()
@@ -129,7 +89,7 @@ elif tp == 'cond_ddpm':
             for x, label in it:
                 num += 1
                 optim.zero_grad()
-                loss = diffusion(x.cuda(), label.cuda())
+                loss = diffusion(x.cuda(), cond=label.cuda())
                 loss.backward()
                 optim.step()
                 it.set_postfix(
@@ -153,7 +113,7 @@ elif tp == 'cond_ddpm':
     eta = 0.
     res = diffusion.sample(shape, cond=cond)
     res = res.detach().cpu().numpy()
-    res1 = diffusion.ddim_sample(shape, eta, cond).detach().cpu().numpy()
+    res1 = diffusion.ddim_sample(shape, eta, cond=cond).detach().cpu().numpy()
     plot_fig(res)
     plot_fig(res1)
     plt.show()
@@ -205,4 +165,115 @@ elif tp == 'mix_gau_ddpm':
     # res1 = diffusion.ddim_sample(shape, eta).detach().cpu().numpy()
     plot_fig(res)
     # plot_fig(res1)
+    plt.show()
+
+
+elif tp == 'cond_mix_gau_ddpm':
+    epochs = 20
+    ema_decay = 0.999
+    update_ema_every = 10
+    p=0.5
+
+
+    model = Cond_Unet(dim=16, channels=1, dim_mults=(1, 2, 4)).cuda()
+    # betas = cosine_beta_schedule(1000, )
+    betas = np.linspace(1e-4, 1e-2, 1000)
+    diffusion = mix_gaussian_ddpm(model, loss_type='l2', betas=betas, phi_start=1., phi_end=0.5, p=p).cuda()
+    ema_model = copy.deepcopy(model)
+    optim = torch.optim.Adam(model.parameters(), lr=1e-3)
+    train_loader, test_loader, val_loader = get_mnist_loaders()
+    ema = EMA(ema_decay)
+
+    num = 0
+    for epoch in range(epochs):
+        with tqdm(train_loader) as it:
+            for x, label in it:
+                num += 1
+                optim.zero_grad()
+                loss = diffusion(x.cuda(), cond=label.cuda())
+                loss.backward()
+                optim.step()
+                it.set_postfix(
+                    ordered_dict={
+                        'train loss': loss.item(),
+                        'epoch': epoch
+                    },
+                    refresh=False
+                )
+
+            if num % update_ema_every == 0:
+                if num < 1000:
+                    ema_model.load_state_dict(model.state_dict())
+                else:
+                    ema.update_model_average(ema_model, model)
+
+    # sample images
+    shape = (36, 1, 28, 28)
+    l = 5
+    cond = (torch.ones(36) * l).cuda()
+    eta = 0.
+    res = diffusion.sample(shape, cond=cond)
+    res = res.detach().cpu().numpy()
+    # res1 = diffusion.ddim_sample(shape, eta, cond=cond).detach().cpu().numpy()
+    plot_fig(res)
+    # plot_fig(res1)
+    plt.show()
+
+
+elif tp == 'snr_gaussian_ddpm':
+    epochs = 10
+    ema_decay = 0.999
+    update_ema_every = 10
+    p = 0.5
+    SNR_min = 4.5397 * 1e-5#1 / (1-(1-1e-2)**2) - 1
+    SNR_max = 9998.3408 #1 / (1-(1-1e-4)**2) - 1
+
+    model = Unet(dim=16, channels=1, dim_mults=(1, 2, 4)).cuda()
+    # diffusion = gaussian_vdm_ddpm(denoise_fn=model,
+    #                               SNR_min=SNR_min,
+    #                               SNR_max=SNR_max,
+    #                               num_steps=1000,
+    #                               device='cuda:0').cuda()
+    diffusion = gau_vdm_ddpm(denoise_fn=model,
+                             snr_min=SNR_min,
+                             snr_max=SNR_max,
+                             num_steps=1000,
+                             device='cuda:0').cuda()
+    ema_model = copy.deepcopy(model)
+    optim = torch.optim.Adam(model.parameters(), lr=1e-3)
+    train_loader, test_loader, val_loader = get_mnist_loaders()
+    ema = EMA(ema_decay)
+
+    num = 0
+    for epoch in range(epochs):
+        with tqdm(train_loader) as it:
+            for x, label in it:
+                num += 1
+                optim.zero_grad()
+                loss = diffusion(x.cuda())
+                loss.backward()
+                optim.step()
+                it.set_postfix(
+                    ordered_dict={
+                        'train loss': loss.item(),
+                        'epoch': epoch
+                    },
+                    refresh=False
+                )
+
+            if num % update_ema_every == 0:
+                if num < 1000:
+                    ema_model.load_state_dict(model.state_dict())
+                else:
+                    ema.update_model_average(ema_model, model)
+
+
+    # sample
+    shape = (36, 1, 28, 28)
+    res = diffusion.sample(shape)
+    res = res.detach().cpu().numpy()
+    # diffusion.get_total_par()
+    # res2 = diffusion.sample_1(shape).detach().cpu().numpy()
+    plot_fig(res)
+    # plot_fig(res2)
     plt.show()
