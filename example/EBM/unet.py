0c7c48542070235636f80db51b30160023741fb1
yantijin <yantijin@163.com>
2021-09-02 16:37:46 +0800

    utils about ebm

--- example/EBM/unet.py
+++ example/EBM/unet.py
@@ -5,6 +5,7 @@ from torch import nn, einsum
 import torch.nn.functional as F
 from inspect import isfunction
 from einops import rearrange
+import numpy as np
 try:
     from apex import amp
     APEX_AVAILABLE = True
@@ -79,6 +80,17 @@ class SinusoidalPosEmb(nn.Module):
         emb = x[:, None] * emb[None, :]
         emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
         return emb
+
+class GaussianFourierProjection(nn.Module):
+  """Gaussian Fourier embeddings for noise levels."""
+
+  def __init__(self, embedding_size=256, scale=1.0):
+    super().__init__()
+    self.W = nn.Parameter(torch.randn(embedding_size) * scale, requires_grad=False)
+
+  def forward(self, x):
+    x_proj = x[:, None] * self.W[None, :] * 2 * np.pi
+    return torch.cat([torch.sin(x_proj), torch.cos(x_proj)], dim=-1)
 
 class Mish(nn.Module):
     def forward(self, x):
@@ -239,8 +251,8 @@ class Unet(nn.Module):
             nn.Conv2d(dim, out_dim, 1)
         )
 
-    def forward(self, x, time):
-        t = self.time_mlp(time) if exists(self.time_mlp) else None
+    def forward(self, x, t):
+        t = self.time_mlp(t) if exists(self.time_mlp) else None
 
         h = []
 
@@ -368,8 +380,8 @@ class Cond_Unet(nn.Module):
             nn.Conv2d(dim, out_dim, 1)
         )
 
-    def forward(self, x, time, cond):
-        t = self.time_mlp(time) if exists(self.time_mlp) else None
+    def forward(self, x, t, cond):
+        t = self.time_mlp(t) if exists(self.time_mlp) else None
         cond = self.cond_mlp(cond) if exists(self.cond_mlp) else None
 
         h = []
@@ -394,6 +406,94 @@ class Cond_Unet(nn.Module):
 
         return self.final_conv(x)
 
+
+class cont_Unet(nn.Module):
+    def __init__(
+        self,
+        dim,
+        out_dim = None,
+        dim_mults=(1, 2, 4, 8),
+        groups = 8,
+        channels = 3,
+        with_time_emb = True
+    ):
+        super().__init__()
+        self.channels = channels
+
+        dims = [channels, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        if with_time_emb:
+            time_dim = dim
+            self.time_mlp = nn.Sequential(
+                GaussianFourierProjection(dim//2),
+                nn.Linear(dim, dim * 4),
+                Mish(),
+                nn.Linear(dim * 4, dim)
+            )
+        else:
+            time_dim = None
+            self.time_mlp = None
+
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out)
+
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append(nn.ModuleList([
+                ResnetBlock(dim_in, dim_out, time_emb_dim = time_dim),
+                ResnetBlock(dim_out, dim_out, time_emb_dim = time_dim),
+                Residual(PreNorm(dim_out, LinearAttention(dim_out))),
+                Downsample(dim_out) if not is_last else nn.Identity()
+            ]))
+
+        mid_dim = dims[-1]
+        self.mid_block1 = ResnetBlock(mid_dim, mid_dim, time_emb_dim = time_dim)
+        self.mid_attn = Residual(PreNorm(mid_dim, LinearAttention(mid_dim)))
+        self.mid_block2 = ResnetBlock(mid_dim, mid_dim, time_emb_dim = time_dim)
+
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.ups.append(nn.ModuleList([
+                ResnetBlock(dim_out * 2, dim_in, time_emb_dim = time_dim),
+                ResnetBlock(dim_in, dim_in, time_emb_dim = time_dim),
+                Residual(PreNorm(dim_in, LinearAttention(dim_in))),
+                Upsample(dim_in) if not is_last else nn.Identity()
+            ]))
+
+        out_dim = default(out_dim, channels)
+        self.final_conv = nn.Sequential(
+            Block(dim, dim),
+            nn.Conv2d(dim, out_dim, 1)
+        )
+
+    def forward(self, x, time):
+        t = self.time_mlp(time) if exists(self.time_mlp) else None
+
+        h = []
+
+        for resnet, resnet2, attn, downsample in self.downs:
+            x = resnet(x, t)
+            x = resnet2(x, t)
+            x = attn(x)
+            h.append(x)
+            x = downsample(x)
+
+        x = self.mid_block1(x, t)
+        x = self.mid_attn(x)
+        x = self.mid_block2(x, t)
+
+        for resnet, resnet2, attn, upsample in self.ups:
+            x = torch.cat((x, h.pop()), dim=1)
+            x = resnet(x, t)
+            x = resnet2(x, t)
+            x = attn(x)
+            x = upsample(x)
+
+        return self.final_conv(x)
 
 if __name__ == "__main__":
     a = torch.randn(32, 3, 28, 28)
---
+++ example/EBM/utils.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_fig(res, path=None):
+    n = 6
+    digit_size = 28
+    figure = np.zeros((28 * n, 28 * n))
+    for i in range(n):
+        for j in range(n):
+            figure[i * digit_size: (i + 1) * digit_size,
+            j * digit_size: (j + 1) * digit_size] = res[n * i + j, 0, :, :]
+
+    plt.figure()
+    plt.imshow(figure, cmap='gray')
+    if path:
+        plt.savefig(path)
+    return figure
+
+def get_mnist_loaders(data_aug=False, batch_size=128, test_batch_size=1000, perc=1.0):
+    if data_aug:
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(28, padding=4),
+            transforms.ToTensor(),
+        ])
+    else:
+        transform_train = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    train_loader = DataLoader(
+        datasets.MNIST(root='../data/', train=True, download=True, transform=transform_train), batch_size=batch_size,
+        shuffle=True, num_workers=8, drop_last=True
+    )
+
+    train_eval_loader = DataLoader(
+        datasets.MNIST(root='../data/', train=True, download=True, transform=transform_test),
+        batch_size=test_batch_size, shuffle=False, num_workers=8, drop_last=True
+    )
+
+    test_loader = DataLoader(
+        datasets.MNIST(root='../data/', train=False, download=True, transform=transform_test),
+        batch_size=test_batch_size, shuffle=False, num_workers=8, drop_last=True
+    )
+
+    return train_loader, test_loader, train_eval_loader
+
+def write_to_file(data, path):
+    with open(path, 'a+') as file_obj:
+        file_obj.write(' '.join([str(i) for i in data]))
+        file_obj.write('\n')
+
+def read_file(path):
+    ls = []
+    with open(path, 'r') as file_obj:
+        for line in file_obj.readlines():
+            ls.append([float(i) for i in line.split(' ')])
+    ls = torch.tensor(np.array(ls), dtype=torch.float32)
+    return ls
