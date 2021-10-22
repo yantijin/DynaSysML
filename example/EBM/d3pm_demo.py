a3be0f50c8a90324e7a1111bc56d5dc401d70b9a
yantijin <yantijin@163.com>
2021-10-22 09:59:52 +0800

    add d3pm demo

---
+++ example/EBM/d3pm_demo.py
@@ -0,0 +1,49 @@
+import torch
+from tqdm import tqdm
+import numpy as np
+from unet import logistic_Unet
+from DynaSysML.EBM.d3pm import categorical_diffusion
+from utils import plot_fig, get_mnist_loaders
+import matplotlib.pyplot as plt
+
+
+# train_loader, test_loader, val_loader = get_mnist_loaders()
+# num = 0
+# for term, _ in train_loader:
+#     if num == 0:
+#         term *= 255
+#         term = term.long()
+#         print(term.shape, term.dtype)
+#         print(term[0], torch.max(term), torch.min(term))
+#         num += 1
+
+epochs = 5
+md = logistic_Unet(dim=16, channels=1, dim_mults=(1, 2, 4)).cuda()
+betas = np.linspace(1e-4, 1e-2, 1000)
+diffusion = categorical_diffusion(betas, transition_bands=None, method='gaussian', num_bits=8,
+                                  loss_type='hybrid', hybrid_coeff=0.001, model_prediction='x_start',
+                                  model_output='logistic_pars').cuda()
+
+optim = torch.optim.Adam(md.parameters(), lr=1e-3)
+train_loader, test_loader, val_loader = get_mnist_loaders()
+
+num = 0
+for epoch in range(epochs):
+    with tqdm(train_loader) as it:
+        for x, label in it:
+            num += 1
+            optim.zero_grad()
+            x *= 255
+            x = x.long().cuda()
+            loss = torch.sum(diffusion.training_losses(md, x))
+            loss.backward()
+            optim.step()
+            it.set_postfix(
+                ordered_dict={'train_loss': loss.item(), 'epoch': epoch},
+                refresh=False
+            )
+
+shape = (36, 1, 28, 28)
+samples = diffusion.p_sample_loop(md, shape, num_timesteps=None)
+plot_fig(samples.detach().cpu().numpy())
+plt.show()
--- example/EBM/unet.py
+++ example/EBM/unet.py
@@ -470,8 +470,8 @@ class cont_Unet(nn.Module):
             nn.Conv2d(dim, out_dim, 1)
         )
 
-    def forward(self, x, time):
-        t = self.time_mlp(time) if exists(self.time_mlp) else None
+    def forward(self, x, t):
+        t = self.time_mlp(t) if exists(self.time_mlp) else None
 
         h = []
 
@@ -495,10 +495,105 @@ class cont_Unet(nn.Module):
 
         return self.final_conv(x)
 
+
+class logistic_Unet(nn.Module):
+    def __init__(
+        self,
+        dim,
+        out_dim = None,
+        dim_mults=(1, 2, 4, 8),
+        groups = 8,
+        channels = 3,
+        with_time_emb = True
+    ):
+        super().__init__()
+        self.channels = channels
+
+        dims = [channels, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        if with_time_emb:
+            time_dim = dim
+            self.time_mlp = nn.Sequential(
+                SinusoidalPosEmb(dim),
+                nn.Linear(dim, dim * 4),
+                Mish(),
+                nn.Linear(dim * 4, dim)
+            )
+        else:
+            time_dim = None
+            self.time_mlp = None
+
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out)
+
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append(nn.ModuleList([
+                ResnetBlock(dim_in, dim_out, time_emb_dim = time_dim),
+                ResnetBlock(dim_out, dim_out, time_emb_dim = time_dim),
+                Residual(PreNorm(dim_out, LinearAttention(dim_out))),
+                Downsample(dim_out) if not is_last else nn.Identity()
+            ]))
+
+        mid_dim = dims[-1]
+        self.mid_block1 = ResnetBlock(mid_dim, mid_dim, time_emb_dim = time_dim)
+        self.mid_attn = Residual(PreNorm(mid_dim, LinearAttention(mid_dim)))
+        self.mid_block2 = ResnetBlock(mid_dim, mid_dim, time_emb_dim = time_dim)
+
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.ups.append(nn.ModuleList([
+                ResnetBlock(dim_out * 2, dim_in, time_emb_dim = time_dim),
+                ResnetBlock(dim_in, dim_in, time_emb_dim = time_dim),
+                Residual(PreNorm(dim_in, LinearAttention(dim_in))),
+                Upsample(dim_in) if not is_last else nn.Identity()
+            ]))
+
+        out_dim = default(out_dim, channels)
+        self.final_conv = nn.Sequential(
+            Block(dim, dim),
+            nn.Conv2d(dim, out_dim*2, 1)
+        )
+
+    def forward(self, x, t):
+        x = x/255. # normalize data
+        t = self.time_mlp(t) if exists(self.time_mlp) else None
+
+        h = []
+
+        for resnet, resnet2, attn, downsample in self.downs:
+            x = resnet(x, t)
+            x = resnet2(x, t)
+            x = attn(x)
+            h.append(x)
+            x = downsample(x)
+
+        x = self.mid_block1(x, t)
+        x = self.mid_attn(x)
+        x = self.mid_block2(x, t)
+
+        for resnet, resnet2, attn, upsample in self.ups:
+            x = torch.cat((x, h.pop()), dim=1)
+            x = resnet(x, t)
+            x = resnet2(x, t)
+            x = attn(x)
+            x = upsample(x)
+
+        res = self.final_conv(x)
+        loc = torch.sigmoid(res[:, :self.channels])
+        log_scale = res[:, self.channels:]
+        return loc, log_scale
+
 if __name__ == "__main__":
     a = torch.randn(32, 3, 28, 28)
     time = torch.randint(0, 1000, (32,),device=a.device).long()
     cond = torch.randint(0, 10, (32,), device=a.device).long()
-    md = Cond_Unet(32, channels=3, dim_mults=(1, 2, 4))
-    res = md(a, time, cond)
-    print(res.shape)
+    # md = Cond_Unet(32, channels=3, dim_mults=(1, 2, 4))
+    md = logistic_Unet(16, channels=3, dim_mults=(1, 2, 4))
+    # res = md(a, time, cond)
+    loc, log_scale = md(a, time)
+    print(loc.shape, log_scale.shape)
